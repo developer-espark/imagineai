37a3d42a8ce90b71d5fd794c6f4d9ac0af797fbc
harsh-patel063 <harsh.patel@esparkbizmail.com>
2025-09-23 11:27:56 +0000

    sorting order in get all weather stats API

--- Backend/src/repository/weather_stats.respository.ts
+++ Backend/src/repository/weather_stats.respository.ts
@@ -8,7 +8,9 @@ export default class WeatherStatsRepo {
 	private readonly msg = new MessageFormation('Weather Stats').message;
 
 	async list() {
-		const data = await WeatherStats.findAll();
+		const data = await WeatherStats.findAll({
+			order:[['updated','DESC']]
+		});
 		
 		return parse(data);
 	}
